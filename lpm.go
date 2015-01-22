9583aaed0504f354fe2a03692bfca4aeafcc2b4d
taylorchu <tailinchu@gmail.com>
2015-01-22 12:27:04 -0800

    clean up

--- lpm.go
+++ lpm.go
@@ -32,21 +32,33 @@ func (this *Matcher) Remove(cs fmt.Stringer) {
 	this.Update(cs, func(interface{}) interface{} { return nil }, false)
 }
 
+func (this *Matcher) prefix(s string, longest bool) (p []string) {
+	for {
+		if _, ok := this.table[s]; ok {
+			p = append(p, s)
+			if longest {
+				break
+			}
+		}
+		idx := strings.LastIndex(s, "/")
+		if idx == -1 {
+			break
+		}
+		s = s[:idx]
+	}
+	return
+}
+
 func (this *Matcher) Update(cs fmt.Stringer, f func(interface{}) interface{}, isPrefix bool) {
 	this.m.Lock()
 	defer this.m.Unlock()
 	s := cs.String()
 	if isPrefix {
-		for {
-			if _, ok := this.table[s]; ok {
-				break
-			}
-			idx := strings.LastIndex(s, "/")
-			if idx == -1 {
-				return
-			}
-			s = s[:idx]
+		p := this.prefix(s, true)
+		if len(p) == 0 {
+			return
 		}
+		s = p[0]
 	}
 	this.table[s] = f(this.table[s])
 	if this.table[s] == nil {
@@ -56,38 +68,23 @@ func (this *Matcher) Update(cs fmt.Stringer, f func(interface{}) interface{}, is
 
 func (this *Matcher) UpdateAll(cs fmt.Stringer, f func(string, interface{}) interface{}) {
 	this.m.Lock()
-	defer this.m.Unlock()
-	s := cs.String()
-	for {
-		if _, ok := this.table[s]; ok {
-			this.table[s] = f(s, this.table[s])
-			if this.table[s] == nil {
-				delete(this.table, s)
-			}
+	for _, s := range this.prefix(cs.String(), false) {
+		this.table[s] = f(s, this.table[s])
+		if this.table[s] == nil {
+			delete(this.table, s)
 		}
-		idx := strings.LastIndex(s, "/")
-		if idx == -1 {
-			return
-		}
-		s = s[:idx]
 	}
+	this.m.Unlock()
 }
 
 func (this *Matcher) Match(cs fmt.Stringer) interface{} {
 	this.m.RLock()
 	defer this.m.RUnlock()
-	s := cs.String()
-	for {
-		if v, ok := this.table[s]; ok {
-			return v
-		}
-		idx := strings.LastIndex(s, "/")
-		if idx == -1 {
-			break
-		}
-		s = s[:idx]
+	p := this.prefix(cs.String(), true)
+	if len(p) == 0 {
+		return nil
 	}
-	return nil
+	return this.table[p[0]]
 }
 
 func (this *Matcher) Visit(f func(string, interface{}) interface{}) {
