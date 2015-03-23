ab095c1cf0c7f33ba1e121b900a9fe8f14d4fe83
taylorchu <tailinchu@gmail.com>
2015-03-23 12:08:34 -0700

    update test cases

--- lpm_test.go
+++ lpm_test.go
@@ -6,23 +6,35 @@ import (
 
 func TestLPM(t *testing.T) {
 	m := NewThreadSafe()
-	m.Add(Key("/1/2/3"), "hello")
-	m.Add(Key("/1/2"), "world")
+	m.Add(Key("/1/2/3"), 123)
+	m.Add(Key("/1/2"), 12)
 	m.Add(Key("/1/2/4"), 124)
 	m.Add(Key("/1/2/5/6"), 1256)
 
-	hello := m.Match(Key("/1/2/3/4"))
-	if hello == nil || hello.(string) != "hello" {
-		t.Fatal("not hello")
-	}
-	world := m.Match(Key("/1/2"))
-	if world == nil || world.(string) != "world" {
-		t.Fatal("not world")
+	num := m.Match(Key("/1/2/3/4"))
+	if num == nil || num.(int) != 123 {
+		t.Fatal("want 123")
 	}
 
 	m.Remove(Key("/1/2/3"))
-	world = m.Match(Key("/1/2/3"))
-	if world == nil || world.(string) != "world" {
-		t.Fatal("should be world", world)
+	num = m.Match(Key("/1/2/3"))
+	if num == nil || num.(int) != 12 {
+		t.Fatal("want 12")
+	}
+
+	m.Update(Key("/1/2/6"), func(interface{}) interface{} {
+		return 126
+	}, true)
+	num = m.Match(Key("/1/2"))
+	if num == nil || num.(int) != 126 {
+		t.Fatal("want 126")
+	}
+
+	m.UpdateAll(Key("/1/2/3"), func(string, interface{}) interface{} {
+		return 1
+	})
+	num = m.Match(Key("/1/2"))
+	if num == nil || num.(int) != 1 {
+		t.Fatal("want 1")
 	}
 }
