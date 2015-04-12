c80a56c48678e97cb2ae88e3178bfdbc3bb62264
taylorchu <tailinchu@gmail.com>
2015-04-12 08:58:02 -0700

    rename mutex

--- thread_safe.go
+++ thread_safe.go
@@ -3,34 +3,34 @@ package lpm
 import "sync"
 
 type threadSafeMatcher struct {
-	u threadUnsafeMatcher
-	sync.RWMutex
+	u  *threadUnsafeMatcher
+	mu sync.RWMutex
 }
 
 func newThreadSafeMatcher() *threadSafeMatcher {
-	return &threadSafeMatcher{u: *newThreadUnsafeMatcher()}
+	return &threadSafeMatcher{u: newThreadUnsafeMatcher()}
 }
 
 func (m *threadSafeMatcher) Update(s string, f func(interface{}) interface{}, lpm bool) {
-	m.Lock()
+	m.mu.Lock()
 	m.u.Update(s, f, lpm)
-	m.Unlock()
+	m.mu.Unlock()
 }
 
 func (m *threadSafeMatcher) UpdateAll(s string, f func(string, interface{}) interface{}) {
-	m.Lock()
+	m.mu.Lock()
 	m.u.UpdateAll(s, f)
-	m.Unlock()
+	m.mu.Unlock()
 }
 
 func (m *threadSafeMatcher) Match(s string, f func(interface{})) {
-	m.RLock()
+	m.mu.RLock()
 	m.u.Match(s, f)
-	m.RUnlock()
+	m.mu.RUnlock()
 }
 
 func (m *threadSafeMatcher) Visit(f func(string, interface{}) interface{}) {
-	m.Lock()
+	m.mu.Lock()
 	m.u.Visit(f)
-	m.Unlock()
+	m.mu.Unlock()
 }
