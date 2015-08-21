e7492ac5f3c9c588e59dbee6dff92022948608d4
taylorchu <tailinchu@gmail.com>
2015-08-21 08:54:22 -0700

    simplify thread_safe

--- thread_safe.go
+++ thread_safe.go
@@ -3,34 +3,34 @@ package lpm
 import "sync"
 
 type threadSafeMatcher struct {
-	u  *threadUnsafeMatcher
-	mu sync.RWMutex
+	Matcher
+	sync.RWMutex
 }
 
 func newThreadSafeMatcher() *threadSafeMatcher {
-	return &threadSafeMatcher{u: newThreadUnsafeMatcher()}
+	return &threadSafeMatcher{Matcher: newThreadUnsafeMatcher()}
 }
 
 func (m *threadSafeMatcher) Update(s string, f func(interface{}) interface{}, exist bool) {
-	m.mu.Lock()
-	m.u.Update(s, f, exist)
-	m.mu.Unlock()
+	m.Lock()
+	m.Matcher.Update(s, f, exist)
+	m.Unlock()
 }
 
 func (m *threadSafeMatcher) UpdateAll(s string, f func(string, interface{}) interface{}, exist bool) {
-	m.mu.Lock()
-	m.u.UpdateAll(s, f, exist)
-	m.mu.Unlock()
+	m.Lock()
+	m.Matcher.UpdateAll(s, f, exist)
+	m.Unlock()
 }
 
 func (m *threadSafeMatcher) Match(s string, f func(interface{}), exist bool) {
-	m.mu.RLock()
-	m.u.Match(s, f, exist)
-	m.mu.RUnlock()
+	m.RLock()
+	m.Matcher.Match(s, f, exist)
+	m.RUnlock()
 }
 
 func (m *threadSafeMatcher) Visit(f func(string, interface{}) interface{}) {
-	m.mu.Lock()
-	m.u.Visit(f)
-	m.mu.Unlock()
+	m.Lock()
+	m.Matcher.Visit(f)
+	m.Unlock()
 }
